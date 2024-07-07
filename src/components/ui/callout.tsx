50c1da5862ecf62d80c0dc3098f67f7144d0c27f
hojin <devbang5674@gmail.com>
2024-07-08 04:49:15 +0900

    feat: 블로그 테스트 포스팅

--- src/components/ui/callout.tsx
+++ src/components/ui/callout.tsx
@@ -1,6 +1,6 @@
 'use client';
 export const Callout = ({ children }) => (
-  <div className="bg-my-gray py-1 px-5 text-base font-medium rounded-xl dark:text-white dark:bg-gray-800">
+  <div className="bg-my-gray py-1 px-5 text-base rounded-xl dark:text-white dark:bg-gray-800">
     {children}
   </div>
 );
