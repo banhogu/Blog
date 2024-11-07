9cf8edf7ac8840c7bd90d5099260c2e268f68abe
hojin <devbang5674@gmail.com>
2024-11-07 16:34:27 +0900

    feat: 글쓴이 컴포넌트 추가

--- app/(post)/layout.tsx
+++ app/(post)/layout.tsx
@@ -1,4 +1,5 @@
 import Finish from '@/components/common/Finish';
+import Writer from '@/components/common/Writer';
 import { PostHeader } from '@/components/post/PostHeader';
 import RecommendPost from '@/components/post/RecommendPost';
 import { getPosts } from '@/utils/getPosts';
@@ -13,7 +14,7 @@ export default async function Layout({ children }) {
     <article className="mb-10 text-gray-800 dark:text-gray-300">
       <PostHeader posts={posts} />
       {children}
-
+      <Writer />
       <RecommendPost posts={posts} />
       <Giscus />
     </article>
